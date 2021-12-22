06c34cd2e815fa6db3cb2c7923d194b74c9171e5
kazkumail <kazkumail@gmail.com>
2021-12-22 07:00:28 -0500

    Updated changes

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from tkinter import ttk
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -30,19 +35,49 @@ def save():
     website = website_entry.get()
     email_username = email_and_username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email_username,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any field empty!")
     else:
+        try:
+            with open("data.json", 'r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
 
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email_username}\nPassword: "
-                                                      f"{password} \n Is it ok to save? ")
+        else:
+            data.update(new_data)
 
-        if is_ok:
-            with open("data.txt", 'a') as data_file:
-                data_file.write(f"{website} | {email_username} | {password}\n")
+            with open('data.json', 'w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message= "No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword:{password}")
+        else:
+            messagebox.showinfo(title="Error", message= f"No details for {website} exists.")
+
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,8 +97,8 @@ website_label.config(text="Website: ")
 website_label.grid(column=0, row=1)
 
 website_entry = Entry()
-website_entry.config(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.config(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_and_username_label = Label()
@@ -71,7 +106,7 @@ email_and_username_label.config(text="Email/Username: ")
 email_and_username_label.grid(column=0, row=2)
 
 email_and_username_entry = Entry()
-email_and_username_entry.config(width=35)
+email_and_username_entry.config(width=40)
 email_and_username_entry.grid(column=1, row=2, columnspan=2)
 email_and_username_entry.insert(0, "@gmail.com")
 
@@ -85,11 +120,18 @@ password_entry.grid(column=1, row=3, columnspan=1)
 
 generate_password_button = ttk.Button()
 generate_password_button.config(text="Generate Password", command=generate_password)
+generate_password_button.config(width= 15)
 generate_password_button.grid(column=2, row=3)
 
 add_button = ttk.Button()
 add_button.config(text="Add")
-add_button.config(width=36, command=save)
+add_button.config(width=37, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = ttk.Button()
+search_button.config(text="Search")
+search_button.config(width=15, command=find_password)
+search_button.grid(column=2, row=1,)
+
 
 window.mainloop()
